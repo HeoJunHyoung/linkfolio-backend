ac953914b986deb2f0eb015b726b9630e097b710
junbro <gjwnsgud4016@gmail.com>
2025-10-21 22:09:07 +0900

    fix: Bean 의존성 순환 관계 문제 해결

--- user-service/src/main/java/com/example/userservice/config/SecurityConfig.java
+++ user-service/src/main/java/com/example/userservice/config/SecurityConfig.java
@@ -36,11 +36,6 @@ public class SecurityConfig {
     private String expirationTime;
 
     @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
         return authConfig.getAuthenticationManager();
     }
