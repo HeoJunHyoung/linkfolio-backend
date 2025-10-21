91dcd1c80666878d1c132e0fdad565ff550e1931
junbro <gjwnsgud4016@gmail.com>
2025-10-21 23:06:15 +0900

    fix: Checker Framework 관련 라이브러리 제거

--- user-service/src/main/java/com/example/userservice/entity/UserEntity.java
+++ user-service/src/main/java/com/example/userservice/entity/UserEntity.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.checkerframework.common.aliasing.qual.Unique;
 
 @Entity
 @Table(name = "`user`")
