7393958ecfb8abbf3947188b5410afd2833f360e
junbro <gjwnsgud4016@gmail.com>
2025-11-10 11:48:37 +0900

    fix: apigateway-service가 데이터베이스 관련 자동 설정을 시도하지 않도록 명시적으로 제외

--- apigateway-service/src/main/java/com/example/apigatewayservice/ApigatewayServiceApplication.java
+++ apigateway-service/src/main/java/com/example/apigatewayservice/ApigatewayServiceApplication.java
@@ -2,8 +2,15 @@ package com.example.apigatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        JpaRepositoriesAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+})  // common-module에 의존하고 있는 apigateway는 common-module이 내부적으로 의존하고 있는 jpa 의존성을 따를 필요가 없어서 exclude 처리 해줬음
 public class ApigatewayServiceApplication {
 
     public static void main(String[] args) {
