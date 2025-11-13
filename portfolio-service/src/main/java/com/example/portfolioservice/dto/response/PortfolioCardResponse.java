533da624b7dd9538da5ad4bda58b8a1519567dfa
junbro <gjwnsgud4016@gmail.com>
2025-11-13 12:17:52 +0900

    chore: hashtags를 리스트 형식으로 JSON 응답에 맞게 반환하도록 수정

--- portfolio-service/src/main/java/com/example/portfolioservice/dto/response/PortfolioCardResponse.java
+++ portfolio-service/src/main/java/com/example/portfolioservice/dto/response/PortfolioCardResponse.java
@@ -6,6 +6,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @Data
@@ -27,4 +29,32 @@ public class PortfolioCardResponse {
 
     private LocalDateTime createdAt;
     private LocalDateTime lastModifiedAt;
+
+    public PortfolioCardResponse(Long userId, Long portfolioId, String name, String email,
+                                 String position, String photoUrl, String oneLiner,
+                                 String hashtags, // DB에서 조회한 String 타입
+                                 Long viewCount, Long likeCount,
+                                 LocalDateTime createdAt, LocalDateTime lastModifiedAt) {
+        this.userId = userId;
+        this.portfolioId = portfolioId;
+        this.name = name;
+        this.email = email;
+        this.position = position;
+        this.photoUrl = photoUrl;
+        this.oneLiner = oneLiner;
+        this.hashtags = stringToHashtagList(hashtags); // 생성자 내부에서 String -> List<String> 변환 수행
+        this.viewCount = viewCount;
+        this.likeCount = likeCount;
+        this.createdAt = createdAt;
+        this.lastModifiedAt = lastModifiedAt;
+    }
+
+    private List<String> stringToHashtagList(String hashtags) {
+        if (hashtags == null || hashtags.isEmpty()) {
+            return Collections.emptyList();
+        }
+        // DB에 쉼표(,)로 저장된 문자열을 분리
+        return Arrays.asList(hashtags.split(","));
+    }
+
 }
