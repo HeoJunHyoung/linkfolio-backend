eb8c5c148da11352d93fd26acb4baa5a57d6bc46
junbro <gjwnsgud4016@gmail.com>
2025-11-23 10:30:58 +0900

    fix: @EnableJpaAuditing 설정 추가

--- support-service/src/main/java/com/example/supportservice/SupportServiceApplication.java
+++ support-service/src/main/java/com/example/supportservice/SupportServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.supportservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SupportServiceApplication {
 
     public static void main(String[] args) {
